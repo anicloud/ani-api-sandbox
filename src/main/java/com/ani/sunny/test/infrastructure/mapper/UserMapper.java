d9e284b8aac6ec577543c76679bdbf7080a5464f
LiHui <812486664@qq.com>
2017-05-26 15:06:59 +0800

    init project

---
+++ src/main/java/com/ani/sunny/test/infrastructure/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.ani.sunny.test.infrastructure.mapper;
+
+import com.ani.sunny.test.infrastructure.mapper.base.SysMapper;
+
+/**
+ * Created by lihui on 17-5-26.
+ */
+public interface UserMapper extends SysMapper{
+}
