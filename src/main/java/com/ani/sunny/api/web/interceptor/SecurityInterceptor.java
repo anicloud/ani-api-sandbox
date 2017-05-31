d148d1635f004406cff1971c33f4e99d5bc9153e
LiHui <812486664@qq.com>
2017-05-31 18:20:14 +0800

    拦截器添加

---
+++ src/main/java/com/ani/sunny/api/web/interceptor/SecurityInterceptor.java
@@ -0,0 +1,24 @@
+package com.ani.sunny.api.web.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by lihui on 17-5-31.
+ */
+public class SecurityInterceptor implements HandlerInterceptor {
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
+        return false;
+    }
+
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+
+    }
+}
